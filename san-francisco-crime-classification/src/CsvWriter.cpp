7dda3e4b7b794bd6cfea7c7b1ed52b92271c8e83
tobiasbianchi <tobias.bianchi93@gmail.com>
2015-11-29 00:12:55 -0300

    mini cambios en reader y writer

--- san-francisco-crime-classification/src/CsvWriter.cpp
+++ san-francisco-crime-classification/src/CsvWriter.cpp
@@ -23,20 +23,7 @@ void CsvWriter::makeSubmitWithMatrix(string path,MatrixXf matrix){
 	vector<string> titulos;
 	string campos = "Id,ARSON,ASSAULT,BAD CHECKS,BRIBERY,BURGLARY,DISORDERLY CONDUCT,DRIVING UNDER THE INFLUENCE,DRUG/NARCOTIC,DRUNKENNESS,EMBEZZLEMENT,EXTORTION,FAMILY OFFENSES,FORGERY/COUNTERFEITING,FRAUD,GAMBLING,KIDNAPPING,LARCENY/THEFT,LIQUOR LAWS,LOITERING,MISSING PERSON,NON-CRIMINAL,OTHER OFFENSES,PORNOGRAPHY/OBSCENE MAT,PROSTITUTION,RECOVERED VEHICLE,ROBBERY,RUNAWAY,SECONDARY CODES,SEX OFFENSES FORCIBLE,SEX OFFENSES NON FORCIBLE,STOLEN PROPERTY,SUICIDE,SUSPICIOUS OCC,TREA,TRESPASS,VANDALISM,VEHICLE THEFT,WARRANTS,WEAPON LAWS\n"; //o lo hago leendo el submit example????*/
 
-	stringstream stream(campos);
-	string campo;
-	while (getline(stream, campo, ',')) {
-		titulos.push_back(campo);
-	}
-	stream.clear();
-	vector<string>::iterator it = titulos.begin();
-	file << titulos.front();
-	it++;
-	for (;it != titulos.end();it++){
-		file << ',' << (*it);
-	}
-
-
+	file << campos;
 
 
 	//data
